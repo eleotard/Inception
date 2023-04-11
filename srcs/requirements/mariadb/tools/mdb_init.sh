4ac7ce14f64fa04420e09c7ffae860f5cc954f85
elsie <eleotard@core>
2023-04-11 15:44:53 +0200

    propre

--- srcs/requirements/mariadb/tools/mdb_init.sh
+++ srcs/requirements/mariadb/tools/mdb_init.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-#set -ex
-
-# 	mysql_install_db --datadir=/var/lib/mysql
-# 	mysqld_safe &
-# 	sleep 2
-
-# 	#mysql -D mysql < "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$MYSQL_ROOT_PASSWORD');FLUSH PRIVILEGES;" | true
-# 	mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-# 	mysql -u root --skip-password -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-# 	mysql -u root --skip-password -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# 	mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# 	mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
-
-# 	#echo "Database created ! "
-
-# 	#service mysql stop | echo -n ""
-# 	mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
-# exec mysqld_safe
-
-
 if [ -d /var/lib/mysql/mysql ]; then 
 	echo "===> $MYSQL_DATABASE already exist !"
 else
@@ -29,11 +8,12 @@ else
 	mysqld_safe &
 	sleep 2
 
-	mysql -u  root  --skip-password << EOF
+	mysql -u root --skip-password << EOF
 		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		DELETE FROM mysql.user WHERE User='';
 		CREATE DATABASE  IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
-		CREATE USER  IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
-		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+		CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
 		FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -7,41 +7,23 @@ then
 	echo "===> wp-config.php already exist <==="
 	sleep 2
 else
-echo "===> create wp-config.php <==== "
-wp core download --locale=fr_FR --allow-root
-sleep 5
+	echo "===> create wp-config.php <==== "
 
-wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=db
-# wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
-wp core install --url="eleotard.42.fr" \
-				--title=Inception \
-				--admin_user=$MYSQL_USER \
-				--admin_password=$MYSQL_PASSWORD \
-				--admin_email=eleotard@student.42.fr \
-				--skip-email \
-				--allow-root
-				
+	wp core download --locale=fr_FR --allow-root
 
-# wp core config --dbname=$MYSQL_DATABASE \
-#   --dbuser=$MYSQL_USER \
-#   --dbpass=$MYSQL_PASSWORD \
-#   --dbhost=$DB_HOST_NAME \
-#   --dbcharset=$WP_CHARSET \
-#   --dbprefix=wp_ \
-#   --dbcollate="utf8_general_ci" \
-#   --allow-root
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST_NAME
 
-    # --dbcharset=$WP_CHARSET \
-    # --dbprefix=wp_ \
-    # --dbcollate=utf8_general_ci \
-    # --dbpass=$MYSQL_PASSWORD \
-    # --dbhost=$DB_HOST_NAME \
-					
-sleep 2
+	wp core install --url="eleotard.42.fr" \
+					--title=Inception \
+					--admin_user=$WP_ADMIN \
+					--admin_password=$WP_ADMIN_PASS \
+					--admin_email=eleotard@student.42.fr \
+					--skip-email \
+					--allow-root
+						
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
 
-
-# wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-
+	sleep 2
 fi
 
 mkdir -p /run/php/7.3/fpm
